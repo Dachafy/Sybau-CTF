cc264c756f35490b2d339ab40227acf799f6d3cc
noob-debian <noob-debian@debian>
2026-03-20 14:20:20 +0700

    setup mvc

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'debian.tailbb3d90.ts.net'
+    ]
+  }
 })
