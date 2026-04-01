4d7c7b1ce091d0268acfa335cb074c88d3e4b226
noob-debian <noob-debian@debian>
2026-04-02 01:26:35 +0700

    .

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, uploadAvatar, setPresetAvatar, removeAvatar } = require('../controllers/userController');
+const { getProfile, setPresetAvatar, removeAvatar } = require('../controllers/userController');
 const { authenticate } = require('../middleware/auth');
 const { uploadAvatar: multerAvatar } = require('../middleware/upload');
 
 router.get('/profile', authenticate, getProfile);
-router.post('/avatar/upload', authenticate, multerAvatar.single('avatar'), uploadAvatar);
 router.post('/avatar/preset', authenticate, setPresetAvatar);
 router.delete('/avatar', authenticate, removeAvatar);
 
